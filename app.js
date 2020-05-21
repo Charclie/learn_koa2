d74a7682b6344bd4cab07f4a22ccea2c8d342283
charcliu <charcliu@tencent.com>
2020-05-21 20:32:06 +0800

    middware

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ import Koa from 'koa'
 const app = new Koa()
 
 app.use(async (ctx, next) => {
+    console.log(`${ctx.request.method} ${ctx.request.url}`);
+    await next()
+})
+
+app.use(async (ctx, next) => {
+    const start = new Date().getTime()
+    if(start > 1){
+        await next()
+    }else {
+        ctx.response.status = 403
+    }
+    const ms = new Date().getTime() - start;
+    console.log(`Time: ${ms}ms`);
+})
+
+app.use(async (ctx, next) => {
     await next()
     ctx.response.type = 'text/html'
     ctx.response.body = '<h1>Hello, Koa2!</h1>'
 })
 
-app.listen(3000)
-console.log('app started at port 3000...');
+app.listen(8080)
+console.log('app started at port 8080...');
