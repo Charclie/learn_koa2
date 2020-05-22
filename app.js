3aeae1a3d20bd9dd2300657fcfba22ed1c8d9a33
charcliu <charcliu@tencent.com>
2020-05-22 23:39:55 +0800

    添加日志中间件

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import bodyParser from 'koa-bodyparser';
 import path from 'path';
 // 静态资源访问
 import serve from 'koa-static';
+import KoaLogger from 'koa-logger';
+import Moment from 'moment';
 import router from './routes/index';
 
 // 1.主页静态网页 把静态页统一放到public中管理
@@ -13,26 +15,38 @@ const home = serve(path.join(`${__dirname}/public/`));
 
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-  console.log(`${ctx.request.method} ${ctx.request.url}`);
-  await next();
-});
+// 模拟日志打印
+// app.use(async (ctx, next) => {
+//   console.log(`${ctx.request.method} ${ctx.request.url}`);
+//   await next();
+// });
 
-app.use(async (ctx, next) => {
-  const start = new Date().getTime();
-  if (start > 1) {
-    await next();
-  } else {
-    ctx.response.status = 403;
-  }
-  const ms = new Date().getTime() - start;
-  console.log(`Time: ${ms}ms`);
-});
+// 模拟计算请求时常中间
+// app.use(async (ctx, next) => {
+//   const start = new Date().getTime();
+//   if (start > 1) {
+//     await next();
+//   } else {
+//     ctx.response.status = 403;
+//   }
+//   const ms = new Date().getTime() - start;
+//   console.log(`Time: ${ms}ms`);
+// });
 
 // 解析post请求
 app.use(bodyParser());
 
+// 静态文件中间件
 app.use(home);
+
+// 日志中间件
+app.use(
+  KoaLogger({
+    transporter: (str) => {
+      console.log(Moment().format('YYYY-MM-DD HH:mm:ss') + str);
+    },
+  }),
+);
 
 // add router middleware
 app.use(router.routes());
